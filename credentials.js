0d4ba620f655a201f1f36debf02d12565b790fa2
Nathan McClain <n8mcclain@gmail.com>
2019-02-16 14:11:11 -0500

    Adding Heroku support

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
 module.exports = {
     //if JWT key goes stale regenerate it by loging onto the website and pull it from v2 RequestHeader Authorization section in the dev console under the network tab
-    manifestAuthKey: "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsInN1YmRvbWFpbiI6ImJvb3phbGxlbiIsImlhdCI6MTU1MDE5NzM1NCwiZXhwIjoxNTUwMjgzNzU0fQ.4ykQuka3VKqXqtIc6e91pphge-TNPhxHKEh3ezKm_Lo"
+    manifestAuthKey: "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsInN1YmRvbWFpbiI6ImJvb3phbGxlbiIsImlhdCI6MTU1MDM0NDExNiwiZXhwIjoxNTUwNDMwNTE2fQ.06q6xhzRIPRSp3BAbfaDmLunPmO-kos6oesBo0dE-JY"
 }
--- manifestNativeConnection.js
+++ manifestNativeConnection.js
@@ -39,9 +39,70 @@ module.exports.queryUsers = (request, response, next) => {
 }
 
 module.exports.queryJobsWithEvidence = (request, response, next) => {
+    var options = {
+        "method": "POST",
+        "hostname": 'boozallen.taqmanifest.com',
+        "path": "/graphql/v2",
+        "headers": {
+          "Content-Type": "application/json",
+          "Authorization": credentials.manifestAuthKey,
+          "cache-control": "no-cache",
+          "Postman-Token": "6beff5c5-c5c2-47f4-9182-2be6f70d2ff8"
+        }
+      };
+      
+      var req = http.request(options, function (res) {
+        var chunks = [];
+      
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+      
+        res.on("end", function () {
+          var body = Buffer.concat(chunks);
+          body = JSON.parse(body.toString());
+          console.log(body.data);
+          
+          response.render('displayJobs', body.data);
 
+        });
+      });
+      
+      
+      req.write(JSON.stringify({ query: 'query($assetId:String,$assetClassId:String,$locationId:String,$completed:Boolean, $itemsPerPage: Int, $pageNumber: Int){jobs(assetId:$assetId,assetClassId:$assetClassId,locationId:$locationId,completed:$completed, itemsPerPage:$itemsPerPage, pageNumber:$pageNumber) {id, assignedUserDetails {id,firstName}, title, priority, creationDate, startDate, status, assignedUser, elapsedTime, completionDate, notes{id,step,note{title,type,text,files{url}}}}}' }));
+      req.end();
 }
 
 module.exports.queryListofLocations = (request, response, next) => {
+    var options = {
+        "method": "POST",
+        "hostname": 'boozallen.taqmanifest.com',
+        "path": "/graphql/v2",
+        "headers": {
+          "Content-Type": "application/json",
+          "Authorization": credentials.manifestAuthKey,
+          "cache-control": "no-cache",
+          "Postman-Token": "6beff5c5-c5c2-47f4-9182-2be6f70d2ff8"
+        }
+      };
+      
+      var req = http.request(options, function (res) {
+        var chunks = [];
+      
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+      
+        res.on("end", function () {
+          var body = Buffer.concat(chunks);
+          body = JSON.parse(body.toString());
+          console.log(body.data);
+          response.render('displayLocations', body.data);
 
+        });
+      });
+      
+      
+      req.write(JSON.stringify({ query: 'query{locations{locationId, name, owner, longitude, latitude, description, locationId, associatedFiles {id, name, fileType}, childLocations{locationId, name, owner, longitude, latitude, description, locationId,childLocations{locationId, name, owner, longitude}}}}' }));
+      req.end();
 }
