ac2601f71e2f6649141dc736af0ae0c046b145b3
Nathan McClain <n8mcclain@gmail.com>
2019-02-19 08:28:50 -0500

    Update token 2/19/19

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
 module.exports = {
     //if JWT key goes stale regenerate it by loging onto the website and pull it from v2 RequestHeader Authorization section in the dev console under the network tab
-    manifestAuthKey: "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsInN1YmRvbWFpbiI6ImJvb3phbGxlbiIsImlhdCI6MTU1MDUyMjMxMSwiZXhwIjoxNTUwNjA4NzExfQ.2QIj3meE11g_f_fkWwGIF9hFQEWhB8SQOs2sK0uzddQ"
+    manifestAuthKey: "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsInN1YmRvbWFpbiI6ImJvb3phbGxlbiIsImlhdCI6MTU1MDU4MjgyNSwiZXhwIjoxNTUwNjY5MjI1fQ.ZND5CVBRkpYyaJvkGkkMdJDCRjenWn4CijnKLliEKF4"
 }
